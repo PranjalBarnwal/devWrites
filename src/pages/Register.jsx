1a6376d32353324511980b19022dece990570c80
Pranjal <happybarnwal007@gmail.com>
2023-05-02 02:35:05 +0530

    updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,7 +27,7 @@
 
 // export default Register
 
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import "./../pages/Register.css";
 import addAvatar from "./../img/addAvatar.png";
 
@@ -40,33 +40,36 @@ const Register = () => {
 
   return (
     <div className="register_container">
-      <div className='register'>
+      <div className="register">
         <div className="register_name">Dev Chats</div>
         <div className="register_text">Register</div>
         <div className="register_details">
-          <input type="text" placeholder='Username'/>
-          <input type="email" placeholder='E-mail'/>
-          <input type="password" placeholder='password'/>
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="password" />
         </div>
-        <div className='add_avatar'>
+        <div className="add_avatar">
           <input
             type="file"
             id="avatar-input"
             accept="image/*"
-            style={{ display: 'none' }}
+            style={{ display: "none" }}
             ref={fileInputRef}
           />
-          <label htmlFor="avatar-input" onClick={handleAvatarClick}>
-            <img src={addAvatar} alt="" />
+          <label htmlFor="file">
+            <img src={addAvatar} alt=""  onClick={handleAvatarClick} />
           </label>
-          <a href="#" onClick={handleAvatarClick}>Add an avatar</a>
+          <a href="#" onClick={handleAvatarClick}>
+            Add an avatar
+          </a>
         </div>
         <button className="signup">Sign Up</button>
-        <div className='already_acc'>Already have an account? <a href="">Login</a></div>
+        <div className="already_acc">
+          Already have an account? <a href="">Login</a>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Register;
-
