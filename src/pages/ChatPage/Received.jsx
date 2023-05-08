9b62880f6b0fea0541b42ad11cdff82584deb9d3
Pranjal <happybarnwal007@gmail.com>
2023-05-08 14:51:17 +0530

    styling done

--- src/pages/ChatPage/Received.jsx
+++ src/pages/ChatPage/Received.jsx
@@ -13,10 +13,11 @@ const Received = () => {
         
         <p>just now</p>
       </div>
-      <div className="received-text"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, voluptas. Iure, hic sed! Culpa similique nulla consequuntur omnis, reprehenderit quod?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, veritatis autem? Iure consequuntur deserunt iste fuga, repellat corrupti illo, a ipsum assumenda earum doloremque voluptate ea accusamus cum ipsam. Eos natus porro necessitatibus, aliquid doloribus nobis suscipit pariatur aspernatur illo saepe laborum tempora ex, dolorum esse enim velit facere, quae voluptate dicta asperiores voluptas corporis veniam libero cupiditate? Possimus maiores reiciendis nisi, saepe mollitia neque vero dolorem assumenda magnam. Adipisci culpa cupiditate asperiores, assumenda sequi dolom amet rerum eos expedita optio, nemo voluptatibus eveniet nesciunt mollitia debitis voluptatum cumque ipsam! Quas doloribus reiciendis eos saepe a dicta nihil, error fugiat magni.</p></div>
+      <div className="received-text"><p>Lorem ipsum dolor,  culpa cupiditate asperiores, assumenda sequi dolom amet rerum eos expedita optio, nemo voluptatibus eveniet nesciunt mollitia debitis voluptatum cumque ipsam! Quas doloribus reiciendis eos saepe a dicta nihil, error fugiat magni.</p></div>
     </div>
     </div>
   );
 };
 
 export default Received;
+
--- src/pages/ChatPage/Sent.jsx
+++ src/pages/ChatPage/Sent.jsx
@@ -5,7 +5,7 @@ const Sent = () => {
   return (
     <div className="recieved-parent">
     <div className="sent-wrapper">
-      <div className="dp-time">
+      <div className="sentdp-time">
         
         <div className="received-dp">
           <img src={dp} alt="" />
@@ -13,7 +13,7 @@ const Sent = () => {
         
         <p>just now</p>
       </div>
-      <div className="sent-text"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, voluptas. Iure, hic sed! Culpa similique nulla consequuntur omnis, reprehenderit quod?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, veritatis autem? Iure consequuntur deserunt iste fuga, repellat corrupti illo, a ipsum assumenda earum doloremque voluptate ea accusamus cum ipsam. Eos natus porro necessitatibus, aliquid doloribus nobis suscipit pariatur aspernatur illo saepe laborum tempora ex, dolorum esse enim velit facere, quae voluptate dicta asperiores voluptas corporis veniam libero cupiditate? Possimus maiores reiciendis nisi, saepe mollitia neque vero dolorem assumenda magnam. Adipisci culpa cupiditate asperiores, assumenda sequi dolom amet rerum eos expedita optio, nemo voluptatibus eveniet nesciunt mollitia debitis voluptatum cumque ipsam! Quas doloribus reiciendis eos saepe a dicta nihil, error fugiat magni.</p></div>
+      <div className="sent-text"><p> debitis voluptatum cumque ipsam! Quas doloribus reiciendis eos saepe a dicta nihil, error fugiat magni.</p></div>
     </div>
     </div>
 
